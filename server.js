e2b2bf51f6e45b06bcf8294a53f568c87390624f
Davor Rakić <kico962@gmail.com>
2025-07-30 10:24:06 +0200

    Without rebuild sqlite3

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const saltRounds = 10;
 const TEMP_PASSWORD = process.env.TEMP_PASSWORD;
 
 // --- Database Setup ---
-const db = new sqlite3.Database(dbPath , (err) => {
+const db = new sqlite3.Database('./corkytube.db' , (err) => {
     if (err) {
         return console.error('Error opening database', err.message);
     }
